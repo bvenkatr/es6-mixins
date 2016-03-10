c41e46543f3ed3d3359023e5adad263a56d7a4ed
Narendra Sisodiya <narendra@narendrasisodiya.com>
2016-03-10 14:22:31 -0800

    initial commit.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-/**
- * Created by narendrasisodiya on 10/03/16.
+/*
+
+ The MIT License (MIT)
+ Copyright (c) 2016 Narendra Sisodiya https://github.com/nsisodiya
+
  */
 
-
-// 2016 - (c) Narendra Sisodiya, narendra@narendrasisodiya.com
-// MIT License
-// Feedback - Twitter me - https://twitter.com/nsisodiya
 
 module.exports = function () {
 	console.log("ES6 + Webpack is Working");
